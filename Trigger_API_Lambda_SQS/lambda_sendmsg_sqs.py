032d5dccc2c3826abd05469eb68833999ab9e707
awsmine <69130541+awsmine@users.noreply.github.com>
2022-08-13 08:35:21 -0400

    Update lambda_sendmsg_sqs.py

--- Trigger_API_Lambda_SQS/lambda_sendmsg_sqs.py
+++ Trigger_API_Lambda_SQS/lambda_sendmsg_sqs.py
@@ -8,7 +8,7 @@ def lambda_handler(event, context):
     now = datetime.now()
     time_date = now.strftime("%H:%M:%S %m/%d/%Y")
     sqs = boto3.client('sqs')
-    sqs.send_message(QueueUrl="https://sqs.us-east-1.amazonaws.com/273978150254/time_queue",MessageBody=time_date)
+    sqs.send_message(QueueUrl="https://sqs.us-east-1.amazonaws.com/xxxxxxxxxxxx/time_queue",MessageBody=time_date)
     
     return {
         'statusCode': 200,
